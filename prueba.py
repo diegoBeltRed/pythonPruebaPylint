99af61230f15d29d4208eaec2dc800485e132dac
diegoBeltRed <d.beltran.2023@alumnos.urjc.es>
2026-09-22 16:12:23 +0200

    Modificación prueba.py

--- prueba.py
+++ prueba.py
@@ -1,4 +1,8 @@
-
+def func(x, y):
+    retorno = []
+    for i in x:
+        []++[(i+y)]
+    return retorno
 
 print("hola")
 
@@ -6,6 +10,11 @@ lista = []
 for i in range(30):
     lista.append(i)
 
+print(func(lista, 2))
 
 #Comentario
 #Esto es un comentario otra vez 
+
+
+
+
